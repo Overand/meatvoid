ea354a605aa76f4aba437d72b2e8d1682214adf7
Overand <Overand@gmail.com>
2017-05-15 17:16:53 -0400

    Boilerplate, initial definitions

---
+++ meatvoid-userscript.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Meat Void
+// @namespace   https://github.com/overand/meatvoid
+// @description Meat Void!
+// @include     *
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var replacedict = new Object(); // Each "Key" is the new euphemism. The values (arrays) are what is replaced
+
+replacedict['meat void'] = ["vagina", "pussy", "cunt", "vag", "twat", "vajayjay", "va jay jay", "vulva",
+                            "coochie", "cooch", "poonany", "poon tang","poon","punany", "axe wound", "cooze",
+                            "bearded clam", "moose knuckle", "vadge", "yoni", "cooter", "muff", "warm center",
+                            "wet center", "chachi", "muffin", "princess parts", "meat curtains", "beef curtains",
+                            "snatch", "hoo-ha", "hoo ha", "box", "tang", "poon tang", 
+                            // These are optional
+                            "clit", "clitoris", "labia", "birth canal", "uterus", "cervix"]
